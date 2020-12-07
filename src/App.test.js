2e7daedb692a42965c87c087055ffd46a49f609d
Tom Hammersley <thomas.j.hammersley@gmail.com>
2020-12-07 16:30:59 +0000

    initialise project, remove unneeded markup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders h1 element", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const h1Element = screen.getByText(/where are you going?/i);
+  expect(h1Element).toBeInTheDocument();
 });
