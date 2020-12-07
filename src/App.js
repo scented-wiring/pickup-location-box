49bd5f46718cb0dfd96b00e23a6051b784d40b69
Tom Hammersley <thomas.j.hammersley@gmail.com>
2020-12-07 22:52:04 +0000

    add focus state & methods, adjust component width

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [focus, setFocus] = useState(false);
+
+  const handleFocus = () => {
+    setFocus(true);
+  };
+
+  const handleBlur = () => {
+    setFocus(false);
+  };
+
   return (
     <div id="searchbox">
       <h1>Where are you going?</h1>
@@ -10,6 +21,8 @@ function App() {
         id="pickup-location"
         name="pickup-location"
         placeholder="city, airport, station, region and district..."
+        onFocus={handleFocus}
+        onBlur={handleBlur}
       ></input>
     </div>
   );
