edffd178ca940d3f365a249e006be31350d3e549
Tom Hammersley <thomas.j.hammersley@gmail.com>
2020-12-07 17:32:41 +0000

    add search widget and rentalcars.com styling

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div id="searchbox">
       <h1>Where are you going?</h1>
+      <label htmlFor="pickup-location">Pick-up Location</label>
+      <input type="text" id="pickup-location" name="pickup-location"></input>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders h1 element", () => {
+test("renders a header", () => {
   render(<App />);
-  const h1Element = screen.getByText(/where are you going?/i);
-  expect(h1Element).toBeInTheDocument();
+  expect(screen.getByText(/where are you going?/i)).toBeInTheDocument();
+});
+
+test("renders a label", () => {
+  render(<App />);
+  expect(screen.getByText("Pick-up Location")).toBeTruthy();
+});
+
+test("renders a textbox", () => {
+  render(<App />);
+  expect(screen.getByRole("textbox")).toBeTruthy();
 });
